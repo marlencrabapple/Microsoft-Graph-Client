625547c4fc64d2b6737b644b8cb6a94fac0af34a
Ian P Bradley <ian.bradley@studiocrabapple.com>
2017-11-27 17:22:32 -0800

    Authentication and tokens implemented

--- lib/Microsoft/Graph/Client.pm
+++ lib/Microsoft/Graph/Client.pm
@@ -9,10 +9,103 @@ use Carp qw(croak);
 use LWP::UserAgent;
 use HTTP::Request::Common qw(GET POST DELETE);
 
+our $api_base_url = 'https://graph.microsoft.com/v1.0';
+
 sub new {
   my ($class, $args) = @_;
 
-  return bless {}, $class;
+  $$args{scope} = join(' ', @ { $$args{scope} })
+    if(ref $$args{scope} eq 'ARRAY');
+
+  croak "Missing scope(s)." unless $$args{scope};
+
+  croak "Missing client ID." unless $$args{client_id};
+  croak "Missing client secret." unless $$args{client_secret};
+  croak "Missing redirect uri." unless $$args{redirect_uri};
+  croak "Missing tenant." unless $$args{tenant};
+
+  my $attribs = {
+    client_id => $$args{client_id},
+    client_secret => $$args{client_secret},
+    tenant => $$args{tenant},
+    scope => $$args{scope}
+  };
+
+  $$attribs{oauth_base_uri} = "https://login.microsoftonline.com/$$args{tenant}/oauth2/v2.0";
+
+  # Assign these after hash creation to avoid values being assigned to wrong keys
+  $$attribs{redirect_uri} = $$args{redirect_uri} if $$args{redirect_uri};
+  $$attribs{login_hint} = $$args{login_hint} if $$args{login_hint};
+
+  my $self = bless {}, $class;
+
+  $$self{attribs} = $attribs;
+  $$self{ua} = LWP::UserAgent->new;
+
+  return $self
+}
+
+sub get_user_authorization_uri {
+  my ($self, $args) = @_;
+
+  my $query_hashref = $args;
+  $$query_hashref{client_id} = $$self{attribs}->{client_id};
+  $$query_hashref{redirect_uri} = $$self{attribs}->{redirect_uri};
+  $$query_hashref{scope} = $$self{attribs}->{scope};
+  $$query_hashref{response_type} = 'code';
+  
+  $$self{'authorization_data'} = { %{ $query_hashref } };
+
+  my $query_string = $self->make_query_string($query_hashref);
+  my $uri = "$$self{attribs}->{oauth_base_uri}/authorize?$query_string";
+
+  return $uri
+}
+
+sub get_tokens {
+  my ($self, $code) = @_;
+
+  croak "Missing authorization code." unless $code;
+
+  my $form_vars = {
+    client_id => $$self{attribs}->{client_id},
+    client_secret => $$self{attribs}->{client_secret},
+    redirect_uri => $$self{attribs}->{redirect_uri},
+    grant_type => 'authorization_code',
+    scope => $$self{attribs}->{scope},
+    code => $code
+  };
+
+  my $req = POST "$$self{attribs}->{oauth_base_uri}/token", $form_vars;
+
+  my ($content, $res) = $self->send_request($req, 1);
+
+  return $content
+}
+
+sub refresh_tokens {
+  my ($self, $refresh_token) = @_;
+
+  croak "Missing refresh token." unless $refresh_token;
+
+  my $form_vars = {
+    client_id => $$self{attribs}->{client_id},
+    client_secret => $$self{attribs}->{client_secret},
+    redirect_uri => $$self{attribs}->{redirect_uri},
+    grant_type => 'refresh_token',
+    refresh_token => $refresh_token,
+    scope => $$self{attribs}->{scope}
+  };
+
+  my $req = POST "$$self->{attribs}->{oauth_base_uri}/token", $form_vars;
+
+  my ($content, $res) = $self->send_request($req, 1);
+
+  return $content
+}
+
+sub list_people {
+  my ($self) = @_;
 }
 
 sub make_query_string {
